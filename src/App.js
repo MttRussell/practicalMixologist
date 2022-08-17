251d2c4803ec30b0475d77b3a3a4c3037b324bc5
MatthewRussell <mttrussell@gmail.com>
2022-08-17 17:54:00 -0400

    fixed userQuery

--- src/App.js
+++ src/App.js
@@ -5,41 +5,32 @@ function App() {
   const [ingredients, setIngredients] = useState([]);
   const [userQuery, setUserQuery] = useState('');
 
-  const handleClick = () => {
-    // return (
+
+  
+    const fetchData= () => {
+      axios({
+        url: `https://api.api-ninjas.com/v1/cocktail?ingredients=${userQuery}`,
+        method: "GET",
+        headers: { "X-Api-Key": "I0byMt2hg8AjzK1rYh2DFA==WrK0dTCyskeA4FbW" },
+        dataResponse: "json",
+        params: {
+          format: "json",
+          
+  
+        },
+      }).then((response) => {
+        //update 'ingredients state with response from API
+        console.log(response.data)
+        setIngredients(response.data);
+      });
+    }
     
-    //   {ingredients.map((ingredients) => {
-    //     return (
-
-    //     );
-    //   })}
-      
-    // )
-  }
-
-  useEffect(() => {
-    const ingredients = "lime";
 
 
 
 
-    axios({
-      url: `https://api.api-ninjas.com/v1/cocktail?ingredients=${ingredients}`,
-      method: "GET",
-      headers: { "X-Api-Key": "I0byMt2hg8AjzK1rYh2DFA==WrK0dTCyskeA4FbW" },
-      dataResponse: "json",
-      params: {
-        format: "json",
-        name: "lemon"
-
-      },
-    }).then((response) => {
-      //update 'ingredients state with response from API
-      console.log(response.data)
-      setIngredients(response.data.ingredients);
-    });
     //Add an empty array here to prevent the callback function from running every time our component re-renders!
-  }, []);
+  
 
   return (
     <div className="App Wrapper">
@@ -47,12 +38,21 @@ function App() {
       <h1>Practical Mixologist</h1>
       <h2>What ingredient is languishing unused in your cupboard? Input below for a selection of cocktails that incorporate it!</h2>
       <div className="searchBar">
-        <input type="search" placeholder="Enter an ingredient to see cocktail recipes!" />
-        <button type="submit" onClick={handleClick}>Submit!</button>
+        <input type="search" value={userQuery} onChange={(event)=> setUserQuery(event.target.value)} placeholder="Enter an ingredient to see cocktail recipes!" />
+        <button type="submit" onClick={fetchData}>Submit!</button>
+        {ingredients.map((ingredient) => {
+          return (
+            <div>
+              <p>{ingredient.name}</p>
+              {/* <p>{ingredient.instructions}</p>
+              {ingredient.ingredients.map((item) => {
+                return <p>{item}</p>;
+              })} */}
+            </div>
+          );
+        })}
       </div>
-      {/* {ingredients.(in) => {
-onChange={(event) => {setUserQuery(event.target.value);}}
-      })} */}
+      
 
 
       <footer>
