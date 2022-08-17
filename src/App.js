9e88f94c5c9f526e390b8cd7cabbda1d22037769
MatthewRussell <mttrussell@gmail.com>
2022-08-17 01:21:48 -0400

    map

--- src/App.js
+++ src/App.js
@@ -2,45 +2,61 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
-const [ingredients, setIngredients] = useState ([]);
-const [userQuery, setUserQuery] = useState ('');
+  const [ingredients, setIngredients] = useState([]);
+  const [userQuery, setUserQuery] = useState('');
+
+  const handleClick = () => {
+    return (
+    
+      {ingredients.map((ingredients) => {
+        return (
+
+        );
+      })}
+      
+    )
+  }
 
   useEffect(() => {
-const ingredients = "sugar";
-    
-    
+    const ingredients = "lime";
+
+
+
+
     axios({
       url: `https://api.api-ninjas.com/v1/cocktail?ingredients=${ingredients}`,
       method: "GET",
-      headers: {"X-Api-Key": "I0byMt2hg8AjzK1rYh2DFA==WrK0dTCyskeA4FbW"},
+      headers: { "X-Api-Key": "I0byMt2hg8AjzK1rYh2DFA==WrK0dTCyskeA4FbW" },
       dataResponse: "json",
-      params: {   
+      params: {
         format: "json"
       },
     }).then((response) => {
       //update 'ingredients state with response from API
-      setIngredients(response.data);
+      setIngredients(response.data.ingredients);
     });
     //Add an empty array here to prevent the callback function from running every time our component re-renders!
   }, []);
 
   return (
-    <div className="App">
+    <div className="App Wrapper">
       <img src="images/old-fashioned.jpeg" alt="" />
       <h1>Practical Mixologist</h1>
       <h2>What ingredient is languishing unused in your cupboard? Input below for a selection of cocktails that incorporate it!</h2>
-      <input type="text" placeholder="Enter an ingredient to see cocktail recipes!"  />
-      <button >Submit!</button>
+      <div className="searchBar">
+        <input type="search" placeholder="Enter an ingredient to see cocktail recipes!" />
+        <button type="submit" onClick={handleClick}>Submit!</button>
+      </div>
       {/* {ingredients.(in) => {
 onChange={(event) => {setUserQuery(event.target.value);}}
       })} */}
-  
-    
-    <footer>
+
+
+      <footer>
         Created by Matthew Russell @ <a href="https://www.junocollege.com">Juno College</a> © 2022
-    </footer>
+      </footer>
     </div>
-    
+
   )
 }
 
